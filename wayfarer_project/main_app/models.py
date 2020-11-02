b4db0ded0d3babed2d923c97c331e1a637774f87
khermalik <khermalik@gmail.com>
2020-11-02 11:01:21 -0800

    Commit KH2 - Created TravelPost and City model

--- wayfarer_project/main_app/models.py
+++ wayfarer_project/main_app/models.py
@@ -13,3 +13,22 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class TravelPost(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    date = models.DateField()
+    likes = models.IntegerField(max_length=20)
+    body = models.TextField()
+    title = models.CharField(max_length=200)
+    image = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
