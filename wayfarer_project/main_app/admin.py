1e478e09e20e04c7be55353bb0b1c29d7b0e0acf
khermalik <khermalik@gmail.com>
2020-11-02 13:47:50 -0800

    Commit KH3 - populates profile with post titles, creates post show page

--- wayfarer_project/main_app/admin.py
+++ wayfarer_project/main_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, TravelPost, City
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(TravelPost)
+admin.site.register(City)
