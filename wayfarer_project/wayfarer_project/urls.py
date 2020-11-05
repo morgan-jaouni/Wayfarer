e1ccf43c6d1627138b9db8280bf24a2726757094
bhowe111 <brendan.howe1@gmail.com>
2020-11-04 22:06:26 -0800

    BH navbar and header, font sizes updated, scrolling issue

--- wayfarer_project/wayfarer_project/urls.py
+++ wayfarer_project/wayfarer_project/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
     path('', include('main_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
